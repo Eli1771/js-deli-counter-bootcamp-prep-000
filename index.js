7770e6508b64bc28c8589cfc728a29b0660f1ce0
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-14 13:45:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name) {
-  var katzDeli = katzDeliLine.push(name);
-  return katzDeli.length;
+function takeANumber(line, name) {
+  line.push(name);
+  var place = line.length - 1;
+  return `Welcome, ${line[line.length]}. You are number ${place} in line.`;
 }
